b3c883aafdb7ff605819f34d37aa1d95e679459e
Nour <44745969+bobnoureldein@users.noreply.github.com>
2023-04-25 19:38:02 +0200

    Update getStart.tsx

--- src/screens/getStart/getStart.tsx
+++ src/screens/getStart/getStart.tsx
@@ -1,11 +1,24 @@
 import React from "react";
-import { View, Text, Image, StyleSheet, ImageBackground } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
+import { StackNavigationProp } from "@react-navigation/stack";
 import RoundedButton from "../../components/RoundedButton/roundButton";
 import Logo from "../../images/logo.jsx";
+import styles from "./styles";
+import { RootStackParamList } from "../../navigation/navigation";
+
 const backgroundImage = require("../../images/unsplash.png");
 
-const GetStart = () => {
+type GetStartScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  "GetStart"
+>;
+
+type Props = {
+  navigation: GetStartScreenNavigationProp;
+};
+
+const GetStart = ({ navigation }: Props) => {
   return (
     <ImageBackground source={backgroundImage} style={styles.background}>
       <LinearGradient
@@ -16,7 +29,7 @@ const GetStart = () => {
           "rgba(255, 255, 255, .7)",
           "rgba(255, 255, 255, 1)",
         ]}
-        style={{ height: "100%", width: "100%" }}
+        style={styles.linear}
       >
         <View style={styles.main}>
           <Logo style={styles.logo} />
@@ -24,40 +37,16 @@ const GetStart = () => {
             <Text style={styles.text}>
               NearBy helps you find the nearest facilities
             </Text>
-            <RoundedButton text="Get Start" icon={true} />
+            <RoundedButton
+              text="Get Start"
+              icon={true}
+              onPress={() => navigation.navigate("Next")}
+            />
           </View>
         </View>
       </LinearGradient>
     </ImageBackground>
   );
 };
-
-const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    resizeMode: "cover",
-  },
-  main: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginBottom: 46,
-  },
-  subContainer: {
-    alignItems: "center",
-  },
-  logo: {
-    marginTop: "30%",
-  },
-
-  text: {
-    fontSize: 32,
-    color: "#000",
-    textAlign: "center",
-    fontWeight: "900",
-    marginBottom: 42,
-    paddingHorizontal: "10%",
-  },
-});
 
 export default GetStart;
