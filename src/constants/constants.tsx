1f839cdfea683926e11fccc7c3097a520a5cf9ef
Nour <44745969+bobnoureldein@users.noreply.github.com>
2023-04-25 19:37:07 +0200

    Update constants.tsx

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,3 +1,6 @@
 import { Dimensions } from "react-native";
 
 export const { height, width } = Dimensions.get("window");
+export const mainColor = "#EF8224";
+export const secondaryColor = "#FDC90D";
+export const backgroundColor = "#F5F5F5";
