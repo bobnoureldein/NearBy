082585e2fdbb7451bb553221b269af4a37eb7577
Nour <44745969+bobnoureldein@users.noreply.github.com>
2023-05-05 13:45:54 +0300

    Create map.tsx

---
+++ src/components/Map/map.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+
+const Map = () => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+          title={"Marker title"}
+          description={"Marker description"}
+        />
+      </MapView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+export default Map;
