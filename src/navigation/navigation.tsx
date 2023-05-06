f80dbcae4fb4b67cf67e60d6698f110673fa01f6
Nour <44745969+bobnoureldein@users.noreply.github.com>
2023-05-06 09:54:41 +0300

    Update navigation.tsx

--- src/navigation/navigation.tsx
+++ src/navigation/navigation.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Text, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { TransitionSpecs, TransitionPresets } from "@react-navigation/stack";
@@ -10,44 +11,137 @@ import Login from "../screens/login/login";
 import VerifyCode from "../screens/verifyCode/verifyCode";
 import CompleteCode from "../screens/complete/complete";
 import Camera from "../screens/camera/camera";
+import Home from "../screens/home/home";
+import ArrowLeft from "../images/arrow-left";
 
-export type RootStackParamList = {
+export type BeforeLoginStackParamList = {
   GetStart: undefined;
   Next: undefined;
   Next2: undefined;
   Next3: undefined;
   Login: undefined;
   VerifyCode: undefined;
-  CompleteCode: undefined;
-  Camera: undefined;
 };
 
-const Stack = createStackNavigator<RootStackParamList>();
+export type AfterLoginStackParamList = {
+  CompleteCode: undefined;
+  Camera: undefined;
+  Home: undefined;
+};
+
+export type CustomHeaderStackParamList = {
+  Home: undefined;
+  Next: undefined;
+};
+
+const BeforeLoginStack = createStackNavigator<BeforeLoginStackParamList>();
+const AfterLoginStack = createStackNavigator<AfterLoginStackParamList>();
+
+const BeforeLoginNavigator = () => {
+  return (
+    <BeforeLoginStack.Navigator
+      screenOptions={{
+        headerShown: false,
+        ...TransitionPresets.ModalFadeTransition,
+        transitionSpec: {
+          open: TransitionSpecs.TransitionIOSSpec,
+          close: TransitionSpecs.TransitionIOSSpec,
+        },
+      }}
+    >
+      <BeforeLoginStack.Screen name="GetStart" component={GetStart} />
+      <BeforeLoginStack.Screen
+        // options={{
+        //   headerShown: true,
+        //   headerTransparent: true,
+        // }}
+        name="Next"
+        component={Next}
+      />
+      <BeforeLoginStack.Screen
+        name="Next2"
+        component={Next2}
+        options={{
+          headerShown: true,
+          headerTransparent: true,
+          headerRight: (props) => <SkipButton {...props} />, // Hide default back button
+          headerTitle: () => null,
+          headerLeft: () => null,
+          // headerStyle: { backgroundColor: "#F9FAFC" },
+          // headerTitleStyle: { color: "#2D3F5F" },
+          // headerTintColor: "#2D3F5F",
+        }}
+      />
+      <BeforeLoginStack.Screen name="Next3" component={Next3} />
+      <BeforeLoginStack.Screen name="Login" component={Login} />
+      <BeforeLoginStack.Screen name="VerifyCode" component={VerifyCode} />
+    </BeforeLoginStack.Navigator>
+  );
+};
+
+function SkipButton({ ...props }) {
+  return <Text {...props}>Skip</Text>;
+}
+
+function BackIcon({ ...props }) {
+  return (
+    <ArrowLeft
+      {...props}
+      style={styles.backStyle}
+      hitSlop={{ top: 12, bottom: 12, left: 20, right: 20 }}
+    />
+  );
+}
+
+const AfterLoginNavigator = () => {
+  return (
+    <AfterLoginStack.Navigator screenOptions={{ headerShown: false }}>
+      <AfterLoginStack.Screen
+        options={{
+          headerShown: false,
+        }}
+        name="CompleteCode"
+        component={CompleteCode}
+      />
+      <AfterLoginStack.Screen
+        options={{
+          headerShown: false,
+        }}
+        name="Camera"
+        component={Camera}
+      />
+      <AfterLoginStack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: true,
+          headerTransparent: true,
+          headerLeft: (props) => <BackIcon {...props} />, // Hide default back button
+          headerTitle: "Select Location",
+          // headerStyle: { backgroundColor: "#F9FAFC" },
+          // headerTitleStyle: { color: "#2D3F5F" },
+          // headerTintColor: "#2D3F5F",
+        }}
+      />
+    </AfterLoginStack.Navigator>
+  );
+};
 
 function App() {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-          ...TransitionPresets.ModalFadeTransition,
-          transitionSpec: {
-            open: TransitionSpecs.TransitionIOSSpec,
-            close: TransitionSpecs.TransitionIOSSpec,
-          },
-        }}
-      >
-        <Stack.Screen name="GetStart" component={GetStart} />
-        <Stack.Screen name="Next" component={Next} />
-        <Stack.Screen name="Next2" component={Next2} />
-        <Stack.Screen name="Next3" component={Next3} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="VerifyCode" component={VerifyCode} />
-        <Stack.Screen name="CompleteCode" component={CompleteCode} />
-        <Stack.Screen name="Camera" component={Camera} />
-      </Stack.Navigator>
+      {isUserLoggedIn ? <AfterLoginNavigator /> : <BeforeLoginNavigator />}
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  backStyle: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: 20,
+  },
+});
 
 export default App;
