ede8ffb4b6cd93bfc1d9e8620b1ef37fa0a9d95b
Nour <44745969+bobnoureldein@users.noreply.github.com>
2023-05-21 13:11:36 +0300

    Update styles.js

--- src/screens/nearMe/styles.js
+++ src/screens/nearMe/styles.js
@@ -14,14 +14,34 @@ const styles = StyleSheet.create({
   topTab: {
     flex: 1,
   },
-  label: {},
+  label: {
+    fontSize: 16,
+    textTransform: "capitalize",
+    color: "#080705",
+    borderRadius: 24,
+  },
   tabBar: {
     width: width * 0.8, // Customize the width of each tab
-    backgroundColor: secondaryColor,
+    backgroundColor: "#fff",
     borderRadius: 24,
     alignSelf: "center",
+    borderColor: secondaryColor,
+    borderWidth: 1,
+    height: 50, // Same as indicator 48 + border width top and bottom each is 1 px
   },
-  indicator: {},
+  indicator: {
+    backgroundColor: secondaryColor,
+    height: 48, // Same as TabBar height minus the border widths
+    borderRadius: 24,
+  },
+  listContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rowContainer: {
+    marginTop: 20,
+    marginHorizontal: 20,
+  },
 });
 
 export default styles;
