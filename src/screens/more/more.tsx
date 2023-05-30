3011ac248894436c4a9502bf9352f99d0f26857d
Nour <44745969+bobnoureldein@users.noreply.github.com>
2023-05-30 22:13:42 +0300

    Create more.tsx

---
+++ src/screens/more/more.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { View } from "react-native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { BottomTabStackParamList } from "../../navigation/navigation";
+import styles from "./styles";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+type MoreScreenNavigationProp = StackNavigationProp<
+  BottomTabStackParamList,
+  "More"
+>;
+
+type Props = {
+  navigation: MoreScreenNavigationProp;
+};
+
+const More = ({ navigation }: Props) => {
+  const insets = useSafeAreaInsets();
+  return (
+    <View style={[styles.container, { paddingTop: insets.top + 48 }]}></View>
+  );
+};
+
+export default More;
