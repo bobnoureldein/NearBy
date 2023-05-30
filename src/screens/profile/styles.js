45ccd75d601497d0d8dcec037080c226b3cef8ee
Nour <44745969+bobnoureldein@users.noreply.github.com>
2023-05-30 22:13:28 +0300

    Create styles.js

---
+++ src/screens/profile/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+import { height, width, mainColor } from "../../constants/constants";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "yellow",
+  },
+});
+
+export default styles;
