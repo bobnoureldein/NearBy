e02f174263793ed52e5ec636d34d64802f36845f
Nour <44745969+bobnoureldein@users.noreply.github.com>
2023-06-07 23:54:15 +0300

    Update styles.js

--- src/screens/more/styles.js
+++ src/screens/more/styles.js
@@ -5,6 +5,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  button: {
+    flexDirection: "row",
+    height: 60,
+    borderRadius: 15,
+    margin: 20,
+    shadowColor: "#a6a6a6",
+    elevation: 6,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  subContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  icon: {
+    height: 24,
+    width: 24,
+    marginHorizontal: 18,
+  },
 });
 
 export default styles;
