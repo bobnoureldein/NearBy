ab736bc27446eb67182aba763e28f1456562eac0
Nour <44745969+bobnoureldein@users.noreply.github.com>
2023-06-07 23:54:20 +0300

    Update styles.js

--- src/screens/privacyPolicy/styles.js
+++ src/screens/privacyPolicy/styles.js
@@ -1,9 +1,21 @@
 import { StyleSheet } from "react-native";
-import { height, width, mainColor } from "../../constants/constants";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginHorizontal: 20,
+  },
+  mainText: {
+    fontSize: 20,
+    fontWeight: "700",
+    marginVertical: 12,
+  },
+  subText: {
+    fontSize: 14,
+  },
+  moreText: {
+    marginTop: 6,
+    color: "#0019C6",
   },
 });
 
