a98af715cf394a0b3429d3f0c5cc17b947e3e2d7
Nour <44745969+bobnoureldein@users.noreply.github.com>
2023-06-07 23:21:30 +0300

    Create styles.js

---
+++ src/screens/privacyPolicy/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+import { height, width, mainColor } from "../../constants/constants";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default styles;
