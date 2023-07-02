bfe17297f2ed345175d7cf8a61473fd584c5c596
Nour <44745969+bobnoureldein@users.noreply.github.com>
2023-07-02 22:58:16 +0300

    Update saved.tsx

--- src/screens/saved/saved.tsx
+++ src/screens/saved/saved.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { View } from "react-native";
+import { View, ScrollView } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { BottomTabStackParamList } from "../../navigation/navigation";
 import styles from "./styles";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import PlaceCard from "../../components/PlaceCard/placeCard";
 
 type SavedScreenNavigationProp = StackNavigationProp<
   BottomTabStackParamList,
@@ -14,10 +15,44 @@ type Props = {
   navigation: SavedScreenNavigationProp;
 };
 
+const data = [
+  { id: 1, label: "Restaurants", image: require("../../images/google.png") },
+  { id: 2, label: "Cafes", image: require("../../images/google.png") },
+  { id: 3, label: "Pharmacy", image: require("../../images/google.png") },
+  { id: 1, label: "Restaurants", image: require("../../images/google.png") },
+  { id: 2, label: "Cafes", image: require("../../images/google.png") },
+  { id: 3, label: "Pharmacy", image: require("../../images/google.png") },
+  { id: 1, label: "Restaurants", image: require("../../images/google.png") },
+  { id: 2, label: "Cafes", image: require("../../images/google.png") },
+  { id: 3, label: "Pharmacy", image: require("../../images/google.png") },
+  { id: 1, label: "Restaurants", image: require("../../images/google.png") },
+  { id: 2, label: "Cafes", image: require("../../images/google.png") },
+  { id: 3, label: "Pharmacy", image: require("../../images/google.png") },
+  { id: 1, label: "Restaurants", image: require("../../images/google.png") },
+  { id: 2, label: "Cafes", image: require("../../images/google.png") },
+  { id: 3, label: "Pharmacy", image: require("../../images/google.png") },
+  { id: 1, label: "Restaurants", image: require("../../images/google.png") },
+  { id: 2, label: "Cafes", image: require("../../images/google.png") },
+  { id: 3, label: "Pharmacy", image: require("../../images/google.png") },
+  { id: 1, label: "Restaurants", image: require("../../images/google.png") },
+  { id: 2, label: "Cafes", image: require("../../images/google.png") },
+  { id: 3, label: "Pharmacy", image: require("../../images/google.png") },
+  { id: 1, label: "Restaurants", image: require("../../images/google.png") },
+  { id: 2, label: "Cafes", image: require("../../images/google.png") },
+  { id: 3, label: "Pharmacy", image: require("../../images/google.png") },
+];
+
 const Saved = ({ navigation }: Props) => {
   const insets = useSafeAreaInsets();
   return (
-    <View style={[styles.container, { paddingTop: insets.top + 48 }]}></View>
+    <ScrollView
+      contentContainerStyle={styles.containerStyle}
+      style={[styles.container, { paddingTop: insets.top + 48 }]}
+    >
+      {data.map((item, index) => (
+        <PlaceCard imageSource={item.image} />
+      ))}
+    </ScrollView>
   );
 };
 
