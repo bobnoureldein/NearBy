52f60a404aa51920a2d39900712a70dc736afcac
Nour <44745969+bobnoureldein@users.noreply.github.com>
2023-07-09 00:20:33 +0300

    Update multiSlider.tsx

--- src/components/MultiSlider/multiSlider.tsx
+++ src/components/MultiSlider/multiSlider.tsx
@@ -1,23 +1,86 @@
 import React, { useState } from "react";
-import MultiSlider from "@ptomasroos/react-native-multi-slider";
+import { Text, View, StyleSheet } from "react-native";
+import MultiSlider, {
+  MultiSliderProps,
+} from "@ptomasroos/react-native-multi-slider";
+import { mainColor, width } from "../../constants/constants";
 
-function App() {
-  // Set initial minimum and maximum values
-  const [minValue, setMinValue] = useState(100);
-  const [maxValue, setMaxValue] = useState(400);
-
-  return (
-    <MultiSlider
-      values={[minValue, maxValue]}
-      min={100}
-      max={400}
-      step={1}
-      onValuesChange={(values) => {
-        setMinValue(values[0]);
-        setMaxValue(values[1]);
-      }}
-    />
-  );
+interface SliderComponentProps {
+  onValuesChange: MultiSliderProps["onValuesChange"];
 }
 
-export default App;
+const SliderComponent: React.FC<SliderComponentProps> = ({
+  onValuesChange,
+}) => {
+  const [values, setValues] = useState([0, 1000]);
+
+  const handleValuesChange = (newValues: number[]) => {
+    setValues(newValues);
+    onValuesChange(newValues);
+  };
+
+  return (
+    <View style={styles.container}>
+      <MultiSlider
+        values={values}
+        sliderLength={width * 0.88}
+        onValuesChange={handleValuesChange}
+        min={0}
+        max={1000}
+        step={1}
+        markerStyle={styles.markerStyle}
+        pressedMarkerStyle={styles.pressedMarkerStyle}
+        selectedStyle={styles.selectedStyle}
+        trackStyle={styles.trackStyle}
+        touchDimensions={{
+          height: 40,
+          width: 40,
+          borderRadius: 20,
+          slipDisplacement: 40,
+        }}
+      />
+      <View style={styles.textContainer}>
+        <Text>00 L.E</Text>
+        <Text>100 L.E</Text>
+        <Text>500 L.E</Text>
+        <Text>1000 L.E</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+  },
+  text: {
+    alignSelf: "flex-start",
+    fontWeight: "700",
+    fontSize: 15,
+  },
+  markerStyle: {
+    backgroundColor: mainColor,
+    borderWidth: 6,
+    borderColor: mainColor,
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+  },
+  pressedMarkerStyle: {
+    backgroundColor: "#fff",
+  },
+  selectedStyle: {
+    backgroundColor: mainColor,
+  },
+  trackStyle: {
+    backgroundColor: "#e0e0e0",
+    height: 4,
+  },
+  textContainer: {
+    width: width * 0.9,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+});
+
+export default SliderComponent;
