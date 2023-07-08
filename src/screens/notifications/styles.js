8a1adb67aba8cfd39696eb9e56f1c33c0f6540f8
Nour <44745969+bobnoureldein@users.noreply.github.com>
2023-07-08 18:16:45 +0300

    Create styles.js

---
+++ src/screens/notifications/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import { height, width, mainColor } from "../../constants/constants";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  containerStyle: {
+    flex: 1,
+  },
+});
+
+export default styles;
